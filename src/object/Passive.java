7bbf81a5e3f7af9b7615ad05c41390060154e020
Starsword7 <startechgaming@gmail.com>
2018-10-14 00:13:51 -0500

    Added running away

--- src/object/Passive.java
+++ src/object/Passive.java
@@ -1,8 +1,11 @@
 package object;
 
+import collision.Moveable;
 import javafx.scene.paint.Color;
 import util.Util;
 import java.util.ArrayList;
+import java.util.List;
+import main.Screen;
 
 public class Passive extends NPC {
 
@@ -16,7 +19,7 @@ public class Passive extends NPC {
     double speed;
     private double lastDX = 1;
     private double lastDY = 0;
-    private double fov = 3; //when they get scared
+    private double fov = 4; //when they get scared
 
     public Passive(double x, double y, double vert, Color color) {
         super(x, y, vert, color);
@@ -43,23 +46,40 @@ public class Passive extends NPC {
                // System.out.println("new target: " + target[0] + "," + target[1]);
               //  System.out.println("position: " + getX() + "," + getY());
                 reached = false;
-            } else {
-                dx = -(getX() - target[0]);
-                dy = -(getY() - target[1]);
+            } 
+            speed = .25;//walking speed
             }
-
-            if ((target[0] - getX()) < threshold && (target[1] - getY()) < threshold) {
+            if (Math.abs(target[0] - getX()) < threshold && Math.abs(target[1] - getY()) < threshold) {
                 reached = true;
                // System.out.println("on target!");
+               //pause!
             } else if (getX() - lastPX == 0 || getY() - lastPY == 0.0) {
                 reached = true; //not reached but time to find new place
                 //System.out.println("wall");
             }
-            speed = .25;//walking speed
-        } else {//scared
-            speed = 1.5;//running speed
-            
-        }
+
+//
+//            
+            double[] go = runWhere();
+           System.out.println("runwhere: "+ go[0]+","+go[1]);
+            if(!(go[0]==1000)&&!(go[1]==1000)){
+                System.out.println("scared");
+//                 System.out.println("scared");
+                scared=true;
+//                //introducing some randomness
+                target[0] = run(go)[0]-(Math.random()/10);
+                target[1] = run(go)[1]+Math.random()/10;
+            }
+            else{
+                scared= false;
+            }
+//            
+        if(scared) {//scared
+           speed = 1.5;//running speed
+//                //still scared?
+                    }
+                dx = -(getX() - target[0]);
+                dy = -(getY() - target[1]);
 
 
         double mag = Math.sqrt(dx * dx + dy * dy);
@@ -85,32 +105,44 @@ public class Passive extends NPC {
             }
         }
 
+    
     }
-
+//
 private double[] runWhere(){
-    int enemies = 0;
+    int enemies  = 0;
     double sumx = 0;
     double sumy = 0;
+    double avx,avy;
     
     //finding average location of all enemies within field of view:
-//    for (int i = 0; i < target.length; i++) { //loop through all objects
-//        if( Math.abs(objects[i].getx-getX())<fov){
-//            if(objects[i].getclass == Enemy){
-//                scared = true;
-//                //add x and y coordinates to sum
-//            }
-//         }
-//        
-//    }
-    double avx = sumx/enemies;
-    double avy = sumy/enemies;
-    return new double[]{avx,avy};
+    List<Objekt> pairs = new ArrayList<>();
+                for(Moveable p1 : Screen.getInstance().getMoveables()){
+                    if(p1 instanceof Enemy){
+                        Objekt enemy = (Objekt)p1;
+                            if(Math.abs(enemy.getX()-getX())<fov && Math.abs(enemy.getY()-getY())<fov){
+                                sumx += enemy.getX();
+                                sumy += enemy.getY();
+                                enemies +=1;
+                                //scared = true;
+                            }
+                        }
+                }    
+    if(enemies>0){
+        avx = sumx/enemies;
+        avy = sumy/enemies;
+    }
+    else{
+        avx = 1000;
+        avy = 1000;
+    }
+        return new double[]{avx,avy};
 }
 
  double[] run(double[] enemies) { //run away  gives direction to run
-        double [] direction = new double[2];
-        
-        return direction;
+        double [] point = new double[2];
+        point[0] = 2*getX()-enemies[0]; 
+        point[1] = 2*getY()-enemies[1];
+        return point;
     }
 
 
